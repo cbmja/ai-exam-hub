f22e3ae5fb25d435a594f9fd7b02721e10092f78
cbmja <cbmja@naver.com>
2025-02-23 17:04:22 +0900

    .index login 완

---
+++ src/main/resources/static/js/layout/navbar.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+//SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar-ar').css('top', '0');
+        } else {
+            $('.navbar-ar').css('top', '40px');
+        }
+    });
+
+
+
+//EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+});
