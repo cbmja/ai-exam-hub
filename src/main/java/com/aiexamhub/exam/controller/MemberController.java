b30492214c30c123ac1aca206989847eda983f34
cbmja <cbmja@naver.com>
2025-02-23 22:05:23 +0900

    .마이페이지

--- src/main/java/com/aiexamhub/exam/controller/MemberController.java
+++ src/main/java/com/aiexamhub/exam/controller/MemberController.java
@@ -2,19 +2,18 @@ package com.aiexamhub.exam.controller;
 
 import com.aiexamhub.exam.dto.Member;
 import com.aiexamhub.exam.service.MemberService;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/ai-exam-hub")
 @RequiredArgsConstructor
-public class LoginController {
+public class MemberController {
 
     private final MemberService memberService;
 
@@ -26,6 +25,17 @@ public class LoginController {
         Member member = memberService.EmailLogin(form , response , model);
 
         return member.getRes();
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(ServletRequest servletRequest , Model model){
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        model.addAttribute("isLogin" , (Boolean)req.getAttribute("isLogin"));
+
+        String memberCode = (String)req.getAttribute("memberCode");
+
+
+        return "/view/mypage/repositories";
     }
 
 }
