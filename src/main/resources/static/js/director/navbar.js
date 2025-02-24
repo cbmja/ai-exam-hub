3083140c27ce4a699e941810481cc9807c50a533
cbmja <cbmja@naver.com>
2025-02-25 02:18:11 +0900

    .추출 페이지 연결

---
+++ src/main/resources/static/js/director/navbar.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+//SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+
+
+$(document).on('click', '#move-my-hub', function(){
+
+    let hubCode = $(this).data('hubcode');
+
+    window.location.href='/ai-exam-hub/mypage/repository/'+hubCode;
+
+});
+
+
+
+//EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+});
