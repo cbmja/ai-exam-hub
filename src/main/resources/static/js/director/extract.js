669eba3cdfa13468bc300e056630d6a64ef4b408
cbmja <cbmja@naver.com>
2025-02-25 23:42:23 +0900

    .logout btn red

--- src/main/resources/static/js/director/extract.js
+++ src/main/resources/static/js/director/extract.js
@@ -218,7 +218,7 @@ $(document).on('click', '#submit-pdf', function(){
     let conAr = $('.content-ar');
     conAr.css('padding-left' , '0');
     conAr.css('width' , 'calc(100% - 315px)');
-    let navStr = `<span id=nav-info data-examnm=${examCateStr} data-subjectnm=${examSubjectStr} data-subjecttype=${examTypeStr}> [ 추출 시험 정보 ] : ${examCateStr} / ${examYearStr} / ${examMonthStr} / ${examSubjectStr} / ${examTypeStr} / ${examOrgStr} / ${originalFileName}</span>`;
+    let navStr = `<span id=nav-info data-examnm=${examCateStr} data-subjectnm=${examSubjectStr} data-subjecttype=${examTypeStr}>${examCateStr} / ${examYearStr} / ${examMonthStr} / ${examSubjectStr} / ${examTypeStr} / ${examOrgStr} / ${originalFileName}</span>`;
     navLeft.append(navStr);
 
     $('#extract-group-name').empty().append(groupNm);
